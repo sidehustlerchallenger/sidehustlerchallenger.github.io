1cdfcbdd74b2ded53cbe36076b1e065473c69a02
sl <s@s.com>
2024-08-30 18:58:43 -0400

    Add Side Hustler Challenger Project

--- src/data/projects.js
+++ src/data/projects.js
@@ -7,6 +7,14 @@ const data = [
     date: '2024-08-01',
     desc: 'The first step to becoming a software engineer',
   },
+  {
+    title: 'The Side Hustler Challenger',
+    subtitle: 'learn to hustle',
+    image: '/images/projects/be-a-software-engineer.png',
+    video: 'https://www.youtube.com/embed/81uXI8Isdfo',
+    date: '2024-08-01',
+    desc: 'The first step to bringing the hustle',
+  },
 ];
 
 export default data;
